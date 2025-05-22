aaf48910a94316b793f1b7049b4b599ce83e9f16
Danny Hua <dannyhua11@gmail.com>
2025-05-22 12:04:01 -0400

    fixed more issues

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  openaiApiKey: process.env.OPENAI_API_KEY ?? '',
+}; 
